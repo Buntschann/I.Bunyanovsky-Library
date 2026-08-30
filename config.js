2e6752a54d702fdde8f7396f644e133cc907dfc9
Buntschann <t5cmt22n5m@privaterelay.appleid.com>
2026-08-31 07:45:11 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Supabaseの設定値を入力してください。
 // SHARED_AUTH_EMAIL はSupabase Authenticationに作成するアプリ内部用の共通アカウントです。
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://tsxckrlvsrevpqtfuhso.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_DLvQObXed8voIrrcujUJkg_V6q8U9YY",
+  SUPABASE_URL: "https://kkrbmrusxrsdntnkuyze.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_030D6WX9ztYZs-bOpZY57Q_J6TM24Ob",
   SHARED_AUTH_EMAIL: "bunchan.flute+i.bunyanovsky-library@gmail.com"
 };
